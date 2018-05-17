68c9827404c2e9e412a86da1714265c7dc5dd066
藤田瞬大朗 <ddt.fujita@gmail.com>
2018-05-17 14:55:59 +0900

    書き込み式SQLドリル　cap1 1-11 問1 push

---
+++ cap1/1_11_1.sql
@@ -0,0 +1,29 @@
+1-11 問1 アンケート回答テーブル(quest)から都道府県別の評価の平均値を求めてみましょう。その際、平均値は小数点以下を四捨五入します。以下の空欄を埋めて、SQL命令を完成させてください。
+
+SELECT 
+  prefecture,
+  ① AS 評価平均 
+FROM
+  quest
+GROUP BY
+②
+;
+
+
+[回答]
+① FLOOR(AVG(answer1))
+② prefecture
+
+
+[出力結果]
+mysql> SELECT prefecture, FLOOR(AVG(answer1)) AS 評価平均 FROM quest GROUP BY prefecture;
++--------------+--------------+
+| prefecture   | 評価平均     |
++--------------+--------------+
+| 茨城県       |            2 |
+| 埼玉県       |            3 |
+| 神奈川県     |            2 |
+| 千葉県       |            3 |
+| 東京都       |            1 |
++--------------+--------------+
+5 rows in set (0.05 sec)
