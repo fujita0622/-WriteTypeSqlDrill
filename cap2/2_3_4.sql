396283500ac686758193ecf78d2ddc98f3f56024
藤田瞬大朗 <ddt.fujita@gmail.com>
2018-06-29 09:48:13 +0900

    書き込み式SQLドリル　cap2 2-3 問4 修正 push

--- cap2/2_3_4.sql
+++ cap2/2_3_4.sql
@@ -1,7 +1,20 @@
 2-3 問4 以下は、書籍目次テーブル(contents)から現在のコンテンツコードと前のコンテンツコードを、現在のコンテンツコードについて昇順で取り出すSQLですが、2つ誤りがあります。
 誤りを指摘してください。
 
+SELECT
+  cp.c_id AS 前のページ,
+  cn.c_id AS 現在のページ
+FROM
+  contents AS cp
+OUTER JOIN
+  contents AS cn
+ON 
+  cp.next_id <> cn.c_id
+ORDER BY 
+  cn.c_id ASC
+;
+
 
 [回答]
-1. 「OUTER JOIN」の前に「LEFT/RIGHT」をつけないと外部結合できない
+1. 結合する両方のテーブルにある情報のみ抜き出したいので「OUTER JOIN」ではなく「INNER JOIN」で内部結合する
 2. 「<>」だとテーブルを統合できないので、正しくは「＝」
