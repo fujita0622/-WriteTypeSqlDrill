17714d808a0fc80810043245c8d45a380be8199c
藤田瞬大朗 <ddt.fujita@gmail.com>
2018-07-04 10:33:28 +0900

    書き込み式SQLドリル　cap2 2-5 問2 修正 push

--- cap2/2_5_2.sql
+++ cap2/2_5_2.sql
@@ -1,4 +1,4 @@
-2-5 問2 所属部署テーブル(depart)から社員テーブル(employee)で使われていない部署コード、部署名を取り出しましょう
+2-5 問2 所属部署テーブル(depart)から社員テーブル(employee)で使われていない部署コード、部署名を取り出しましょう。
 以下の空欄を埋めて、SQL命令を完成させなさい。
 
 SELECT 
@@ -20,13 +20,31 @@ WHERE
 
 
 [回答]
-① depart AS d
+① depart
 ② NOT EXISTS
-③ d.depart_id = e.depart_id
+③ depart.depart_id = employee.depart_id
 
 
 [出力結果]
- depart_id | depart_name 
------------+-------------
- E02       | 第二営業部
-(1 row)
+mysql> SELECT 
+    ->   depart_id,
+    ->   depart_name
+    -> FROM
+    ->   depart
+    -> WHERE
+    ->   NOT EXISTS
+    -> (
+    -> SELECT
+    ->   *
+    -> FROM
+    ->   employee
+    -> WHERE
+    ->   depart.depart_id = employee.depart_id
+    -> )
+    -> ;
++-----------+-----------------+
+| depart_id | depart_name     |
++-----------+-----------------+
+| E02       | 第二営業部      |
++-----------+-----------------+
+1 row in set (0.04 sec)
